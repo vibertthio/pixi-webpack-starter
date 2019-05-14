c7a4f86ba0122e1dedcf9e2c93a5601488c3a038
Vibert Thio <vibertthio@gmail.com>
2019-05-14 20:34:58 +0800

    :hammer: add gh-pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import * as PIXI from 'pixi.js';
 
 import './index.scss';
-import bunnyImg from './assets/bunny.png';
 import venusImg from './assets/venus_1000.jpg';
+import * as dat from 'dat.gui';
 
-// setup
+// gui
+const gui = new dat.GUI();
+
+// setup app
 const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight,
@@ -18,36 +21,20 @@ console.log(PIXI.utils.isWebGLSupported());
 console.log(PIXI.RENDERER_TYPE);
 console.log(app.renderer.type);
 
-// use image
-const bunny = PIXI.Sprite.from(bunnyImg);
-bunny.anchor.set(0.5);
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
-app.stage.addChild(bunny);
-
 // texture
 const venusTexture = PIXI.Texture.from(venusImg);
 const { baseTexture, frame } = venusTexture;
-const crop = new PIXI.Rectangle(
-  venusTexture.frame.x,
-  venusTexture.frame.y,
-  venusTexture.frame.width * 0.5,
-  venusTexture.frame.height * 0.5,
-);
-const trim = new PIXI.Rectangle(
-  venusTexture.frame.x + 100,
-  venusTexture.frame.y + 100,
-  venusTexture.frame.width * 0.3,
-  venusTexture.frame.height * 0.3,
-);
-const rotatedTexture = new PIXI.Texture(baseTexture, frame, crop, trim, 0);
+const texturePixelRatio = frame.height / frame.width;
+const orig = frame;
+const trim = frame;
+const anchor = new PIXI.Point(0, 0);
+const rotatedTexture = new PIXI.Texture(baseTexture, frame, orig, trim, 0, anchor);
 const venusSprite = new PIXI.TilingSprite(rotatedTexture, window.innerWidth, window.innerHeight);
-venusSprite.anchor.set(0);
 app.stage.addChild(venusSprite);
+console.log(app.stage.children);
 
 // animation
-app.ticker.add((delta) => {
-  bunny.rotation += 0.1 * delta;
+app.ticker.add(() => {
 });
 
 // resize
