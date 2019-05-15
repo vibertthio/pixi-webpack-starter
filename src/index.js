41762d1a08e2c181437b54a7af715a2ee21b1f3c
Vibert Thio <vibertthio@gmail.com>
2019-05-15 10:27:28 +0800

    :lipstick: make the background image fullscreen

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.scss';
 import venusImg from './assets/venus_1000.jpg';
 import * as dat from 'dat.gui';
 
+// variables
+let loading = true;
+let texture;
+let sprite;
+let spriteDisplayRatio;
+
 // gui
 const gui = new dat.GUI();
 
@@ -21,25 +27,52 @@ console.log(PIXI.utils.isWebGLSupported());
 console.log(PIXI.RENDERER_TYPE);
 console.log(app.renderer.type);
 
-// texture
-const venusTexture = PIXI.Texture.from(venusImg);
-const { baseTexture, frame } = venusTexture;
-const texturePixelRatio = frame.height / frame.width;
-const orig = frame;
-const trim = frame;
-const anchor = new PIXI.Point(0, 0);
-const rotatedTexture = new PIXI.Texture(baseTexture, frame, orig, trim, 0, anchor);
-const venusSprite = new PIXI.TilingSprite(rotatedTexture, window.innerWidth, window.innerHeight);
-app.stage.addChild(venusSprite);
-console.log(app.stage.children);
+// load
+app.loader
+  .add(venusImg)
+  .load(setup);
+
+function setup(loader, resources) {
+  // texture
+  const venusTexture = resources[venusImg].texture;
+  const { baseTexture, frame } = venusTexture;
+  const texturePixelRatio = frame.height / frame.width;
+  const orig = frame;
+  const trim = frame;
+  const anchor = new PIXI.Point(0, 0);
+  const rotatedTexture = new PIXI.Texture(baseTexture, frame, orig, trim, 0, anchor);
+  const venusSprite = new PIXI.Sprite(rotatedTexture);
+  venusSprite.anchor.set(0.5);
+
+  app.stage.addChild(venusSprite);
+  sprite = venusSprite;
+  texture = rotatedTexture;
+  spriteDisplayRatio = texture.frame.width / texture.frame.height;
+  loading = false;
+}
 
 // animation
 app.ticker.add(() => {
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const ratio = width / height;
+
+  if (!loading) {
+    // fullscreen
+    sprite.x = window.innerWidth * 0.5;
+    sprite.y = window.innerHeight * 0.5;
+    sprite.scale.set(
+      (ratio > spriteDisplayRatio) ?
+      (width / texture.frame.width) :
+      (height / texture.frame.height)
+    );
+  }
 });
 
 // resize
 window.onresize = function (e) {
   var w = window.innerWidth;
-  var h = window.innerHeight;
+  const h = window.innerHeight;
   app.renderer.resize(w, h);
 };
